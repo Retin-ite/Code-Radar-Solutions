59b04acf2a8fec3a16e526d92f847a445523c354
Retin-ite <jaat6475@gmail.com>
2025-02-14 20:41:19 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
-int main(){
-    double a,b;
+
+int main() {
+    double a, b, result;
     char c;
-    scanf("%lf %lf",&a,&b);
-    getchar();
-    scanf("%c",&c);
-    if (c=='+'){
-        printf("%d",a+b);
-        
-    }else if(c== '-'){
-        printf("%d",a-b);
-    } else if(c=='*'){printf("%d",a*b);}
-    else if(c== '/'){if (b!=0)printf("%d",a/b);
-    else{printf("Error")};}
-    else{printf("Error");}
+
+    // Take input
+    scanf("%lf %lf", &a, &b);
+    getchar();  // Consume newline
+    scanf("%c", &c);
+
+    // Perform calculation
+    if (c == '+')
+        result = a + b;
+    else if (c == '-')
+        result = a - b;
+    else if (c == '*')
+        result = a * b;
+    else if (c == '/') {
+        if (b != 0)
+            result = a / b;
+        else {
+            printf("Error");
+            return 0;
+        }
+    } else {
+        printf("Error");
+        return 0;
+    }
+
+    // Print as integer if whole number, else with decimal
+    if (result == (int)result)
+        printf("%d", (int)result);
+    else
+        printf("%.2lf", result);
+
+    return 0;
 }
